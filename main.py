- FIxed another lowercase variable - Added timer for current time elapsed and average for each training fold - Changed the verbose messages to be more intuitive

main.py
@@ -48,6 +48,8 @@ acc_s = []
 times = []
 params_r = []
 
+t_start_total = perf_counter()  
+
 for a in epochs_arr:
     for b in batch_count_arr:
         for c in conv_layers_count_arr:
@@ -63,12 +65,19 @@ for a in epochs_arr:
                                             'FinalFeatVec':g
                                             }, z, X_shape)
 
-                            pipeline = pipeline(X, y, z, cur_dataset.name, None, cnn_s)
-                            results = pipeline.train(num_val_splits, a, b)
+                            obj_set = {'epochs': a, 'batch_count': b, 'ConvCount': c, 'ConvFilterSizes': str(d), \
+                                            'ConvFilterCount': str(e), 'FirstDenseLayer':f, 'FinalFeatVec':g}
+                            print("current config {0}".format(str(obj_set)))
+                            pipeline = Pipeline(X, y, z, cur_dataset.name, None, cnn_s)
+                            results = pipeline.train(num_val_splits, 1, 1, a, b)
+
+                            t_cur_total = perf_counter()
+                            t_cur_total_elapsed = t_cur_total - t_start_total
+                            print('cur time elapsed hours{0}, mins{1}, secs{2}',format(t_cur_total_elapsed/60/60, t_cur_total_elapsed/60, t_cur_total_elapsed))
+
                             acc_s.append(results[1])
                             times.append(results[2])
-                            params_r.append({'epochs': a, 'batch_count': b, 'ConvCount': c, 'ConvFilterSizes': str(d), \
-                                            'ConvFilterCount': str(e), 'FirstDenseLayer':f, 'FinalFeatVec':g})     
+                            params_r.append(obj_set)     
 
 
 
@@ -81,7 +90,7 @@ for a in epochs_arr:
 #sn.heatmap(results[0], annot=True, annot_kws={"size": 10})
 #plt.show()
 
-with open("results/res1", 'wb') as f:
+with open("results/res2", 'wb') as f:
     np.savez(f, acc_s, times, params_r)
 
 

pipeline.py
@@ -28,23 +28,24 @@ class Pipeline:
 
     def train(self, folds = 10, num_epochs_pre = 1, batch_count_pre = 1, num_epochs_post = 1, batch_count_post = 1):
         
-        t_start_total_elapsed = perf_counter()  
+        self.__folds = folds
+        times_split = []
+
         preproc = False
         if(self.__prepro != None):
             preproc = True
 
-        labels = list(range(0,self.__z))
+        #labels = list(range(0,self.__z))
         conf_mat = np.zeros((self.__z,self.__z))
         acc_s = 0
 
         skf = StratifiedKFold(n_splits=folds, shuffle=True)
         i = 0
 
-
-
         for train_split, test_split in skf.split(self.__X, self.__y):
             #if(preproc):
             #    self.__prepro.reinit()
+
             self.__clear_folder('temp_data/')
             self.__save_train_test_split(i, train_split, test_split)
         
@@ -52,7 +53,11 @@ class Pipeline:
                 self.__train_epoch_split(num_epochs_pre, i, batch_count_pre, preproc)
                 self.__clear_folder('temp_data/batches/')
 
+            t_start_train_elapsed = perf_counter()
             self.__train_epoch_split(num_epochs_post, i, batch_count_post, preproc, True)
+            t_end_train_elapsed = perf_counter()
+
+            times_split.append(t_end_train_elapsed - t_start_train_elapsed)
 
             post_X_test, post_y_test = self.__load_split(i, False, preproc)
             y_pred = self.__classifier.predict(post_X_test)
@@ -62,17 +67,14 @@ class Pipeline:
             #conf_mat += res_c
 
             i+=1
-
             self.__classifier.reinit()
-        
-        t_end_total_elapsed = perf_counter()
 
+    
         acc_s /= folds
+        
+        time_mins_avg_train_elapsed = mean(times_split) / 60
 
-
-        time_mins_total_elapsed = (t_end_total_elapsed - t_start_total_elapsed) / 60
-
-        return [conf_mat, acc_s, time_mins_total_elapsed]
+        return [conf_mat, acc_s, time_mins_avg_train_elapsed]
 
 
     def __train_epoch_split(self, num_epochs, tr_split_ind, batch_count, preproc, shuffle = False):
@@ -106,8 +108,8 @@ class Pipeline:
                 target = self.__prepro if preproc else self.__classifier
                 res = target.train_model(X_batch, y_batch)
 
-                print("batch {0}/{1}".format(j+1,batch_count), end="\r", flush=True)
-            print("epoch {0}/{1}".format(e+1,num_epochs), end="\r", flush=True)
+                print("val_split {0}/{1} epoch {2}/{3} batch {4}/{5}   ".format(tr_split_ind+1, self.__folds, e+1, num_epochs, j+1,batch_count), end="\r", flush=True)
+            
         
     def __clear_folder(self, folder):
         for parent, dirnames, filenames in os.walk(folder):